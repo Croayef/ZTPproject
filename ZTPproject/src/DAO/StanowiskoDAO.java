971e7c567d5559cdd5f83c22f6c19741d6b14795
Artur Pogorzelski <pogorzelski.a@gmail.com>
2017-01-12 23:14:48 +0100

    KOMIT

--- ZTPproject/src/DAO/StanowiskoDAO.java
+++ ZTPproject/src/DAO/StanowiskoDAO.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author Artur
- */
-public class StanowiskoDAO {
-    
+import DAOInterfaces.StanowiskoDAOInterface;
+import POJO.Stanowisko;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+public class StanowiskoDAO implements StanowiskoDAOInterface {
+
+    public void addStanowisko(Stanowisko stanowisko, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(stanowisko);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeStanowisko(int id, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Stanowisko stanowisko = (Stanowisko) session.get(Stanowisko.class, id);
+        session.delete(stanowisko);
+        transaction.commit();
+        session.close();
+    }
+
+    public void editStanowisko(Stanowisko stanowisko, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(stanowisko);
+        transaction.commit();
+        session.close();
+    }
+
+    public Stanowisko getStanowisko(int id, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Stanowisko stanowisko = (Stanowisko) session.get(Stanowisko.class, id);
+        transaction.commit();
+        session.close();
+        return stanowisko;
+    }
+
+    public List<Stanowisko> getEachStanowisko(SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Stanowisko> positions = (List<Stanowisko>) session.createCriteria(Stanowisko.class).list();
+        transaction.commit();
+        session.close();
+        return positions;
+    }
+
 }
--- ZTPproject/src/DAOInterfaces/StanowiskoDAOInterface.java
+++ ZTPproject/src/DAOInterfaces/StanowiskoDAOInterface.java
@@ -1,10 +1,19 @@
 package DAOInterfaces;
 
+import POJO.Stanowisko;
+import java.util.List;
+
+import org.hibernate.SessionFactory;
 
 public interface StanowiskoDAOInterface {
-    public void addStanowisko();
-    public void removeStanowisko();
-    public void editStanowisko();
-    public void getStanowisko();
-    public void getEachStanowisko();    
+
+    public void addStanowisko(Stanowisko stanowisko, SessionFactory sessionFactory);
+
+    public void removeStanowisko(int id, SessionFactory sessionFactory);
+
+    public void editStanowisko(Stanowisko stanowisko, SessionFactory sessionFactory);
+
+    public Stanowisko getStanowisko(int id, SessionFactory sessionFactory);
+
+    public List<Stanowisko> getEachStanowisko(SessionFactory sessionFactory);
 }
