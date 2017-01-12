e6d2d889403ec203d6cfc963f436c621c484c1f5
Artur Pogorzelski <pogorzelski.a@gmail.com>
2017-01-12 22:37:09 +0100

    KOMIT

--- ZTPproject/src/DAO/FilmDAO.java
+++ ZTPproject/src/DAO/FilmDAO.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DAO;
 
-/**
- *
- * @author Artur
- */
-public class FilmDAO {
-    
+import DAOInterfaces.FilmDAOInterface;
+import POJO.Film;
+import java.util.List;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.Session;
+
+public class FilmDAO implements FilmDAOInterface {
+
+    public void addFilm(Film film, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(film);
+        transaction.commit();
+        session.close();
+    }
+
+    public void removeFilm(int id, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Film film = (Film) session.get(Film.class, id);
+        session.delete(film);
+        transaction.commit();
+        session.close();
+    }
+
+    public void editFilm(Film film, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(film);
+        transaction.commit();
+        session.close();
+    }
+
+    public Film getFilm(int id, SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Film film = (Film) session.get(Film.class, id);
+        transaction.commit();
+        session.close();
+        return film;
+    }
+
+    public List<Film> getEachFilm(SessionFactory sessionFactory) {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+        List<Film> movies = (List<Film>) session.createCriteria(Film.class).list();
+        transaction.commit();
+        session.close();
+        return movies;
+    }
+
 }
--- ZTPproject/src/DAOInterfaces/FilmDAOInterface.java
+++ ZTPproject/src/DAOInterfaces/FilmDAOInterface.java
@@ -1,9 +1,13 @@
 package DAOInterfaces;
 
+import POJO.Film;
+import java.util.List;
+import org.hibernate.SessionFactory;
+
 public interface FilmDAOInterface {
-    public void addFilm();
-    public void removeFilm();
-    public void editFilm();
-    public void getFilm();
-    public void getEachFilm();
+    public void addFilm(Film film, SessionFactory sessionFactory);
+    public void removeFilm(int id, SessionFactory sessionFactory);
+    public void editFilm(Film film, SessionFactory sessionFactory);
+    public Film getFilm(int id, SessionFactory sessionFactory);
+    public List<Film> getEachFilm(SessionFactory sessionFactory);
 }
