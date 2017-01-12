d458bba52f997b68c9e6933302d60d6a016c12c2
menf <menfis666@gmail.com>
2017-01-12 23:12:56 +0100

    TRANSACTION DAO IMPLEMENTATION

--- ZTPproject/src/DAO/TransakcjaDAO.java
+++ ZTPproject/src/DAO/TransakcjaDAO.java
@@ -5,10 +5,66 @@
  */
 package DAO;
 
+import DAOInterfaces.TransakcjaDAOInterface;
+import POJO.Transakcja;
+import java.util.List;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 /**
  *
  * @author Artur
  */
-public class TransakcjaDAO {
+public class TransakcjaDAO implements TransakcjaDAOInterface {
+private Session session;
+private Transaction transaction;
+    @Override
+    public void addTransakcja(Transakcja transakcja, SessionFactory sessionFactory) {
+    session= sessionFactory.getCurrentSession();
+    transaction = session.beginTransaction();
+    session.save(transakcja);
+    transaction.commit();
+    session.close();
+    }
+
+    @Override
+    public void removeTransakcja(int id, SessionFactory sessionFactory) {
+         session= sessionFactory.getCurrentSession();
+         transaction = session.beginTransaction();
+        Transakcja transakcja = (Transakcja) session.get(Transakcja.class, id);
+        session.delete(transakcja);
+        transaction.commit();
+        session.close();
+    }
+
+    @Override
+    public void editTransakcja(Transakcja transakcja, SessionFactory sessionFactory) {
+         session= sessionFactory.getCurrentSession();
+         transaction = session.beginTransaction();
+         session.update(transakcja);
+         transaction.commit();
+         session.close();
+    }
+
+    @Override
+    public Transakcja getTransakcja(int id, SessionFactory sessionFactory) {
+        session= sessionFactory.getCurrentSession();
+        transaction = session.beginTransaction();
+        Transakcja transakcja = (Transakcja) session.get(Transakcja.class, id);
+        transaction.commit();
+        session.close();
+        return transakcja;
+    }
+
+    @Override
+    public List<Transakcja> getEachTransakcja(SessionFactory sessionFactory) {
+        session = sessionFactory.openSession();
+        transaction = session.beginTransaction();
+        List<Transakcja> transactions = (List<Transakcja>) session.createCriteria(Transakcja.class).list();
+        transaction.commit();
+        session.close();
+        return transactions;
+    }
     
 }
--- ZTPproject/src/DAOInterfaces/TransakcjaDAOInterface.java
+++ ZTPproject/src/DAOInterfaces/TransakcjaDAOInterface.java
@@ -1,9 +1,13 @@
 package DAOInterfaces;
 
+import POJO.Transakcja;
+import java.util.List;
+import org.hibernate.SessionFactory;
+
 public interface TransakcjaDAOInterface {
-    public void addTransakcja();
-    public void removeTransakcja();
-    public void editTransakcja();
-    public void getTransakcja();
-    public void getEachTransakcja();     
+    public void addTransakcja(Transakcja transakcja, SessionFactory sessionFactory);
+    public void removeTransakcja(int id, SessionFactory sessionFactory);
+    public void editTransakcja(Transakcja transakcja, SessionFactory sessionFactory);
+    public Transakcja getTransakcja(int id, SessionFactory sessionFactory);
+    public List<Transakcja> getEachTransakcja(SessionFactory sessionFactory);     
 }
