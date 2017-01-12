c71e44451a80165052d8f1c8c2c5702bc919250a
menf <menfis666@gmail.com>
2017-01-12 21:28:41 +0100

    DAO INTERFACES IMPL

--- ZTPproject/src/DAO/AdresDAO.java
+++ ZTPproject/src/DAO/AdresDAO.java
@@ -1,7 +1,34 @@
 package DAO;
 
-public class AdresDAO {
-    //test
-    //test_2
-    int abc;
+import DAOInterfaces.AdresDAOInterface;
+import org.hibernate.SessionFactory;
+
+
+public class AdresDAO implements AdresDAOInterface{
+
+    @Override
+    public void addAdres() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+ 
+    @Override
+    public void removeAdres(int id, SessionFactory session) {
+       session.getCurrentSession()
+    }
+
+    @Override
+    public void editAdres() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getAdres() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getEachAdres() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 }
