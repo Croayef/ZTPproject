ec40311b4ee344fe16dcef48a2b68ebc964dc414
Artur Pogorzelski <pogorzelski.a@gmail.com>
2017-01-14 19:15:43 +0100

    Main window

--- ZTPproject/src/windows/MainWindow.java
+++ ZTPproject/src/windows/MainWindow.java
@@ -6,14 +6,10 @@ import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import javax.swing.JButton;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class MainWindow extends JFrame {
 
