bd599277fd8b0b7dca4efeca4100e16286697b05
Artur Pogorzelski <pogorzelski.a@gmail.com>
2017-01-15 02:16:08 +0100

    Coś zepsułem, coś naprawiłem

--- ZTPproject/src/windows/ShowMovieWindow.java
+++ ZTPproject/src/windows/ShowMovieWindow.java
@@ -9,50 +9,50 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
-public class ShowMovieWindow  extends JPanel implements CustomWindowInterface {
+public class ShowMovieWindow extends JPanel implements CustomWindowInterface {
+
     private JTable moviesTable;
     private JScrollPane scrollPane;
     private JButton showMovieButton;
     private static ShowMovieWindow instance;
-    
-        public ShowMovieWindow() {
-            
-            scrollPane = new JScrollPane();
-            showMovieButton = new JButton("Usuń film");
-            moviesTable = new javax.swing.JTable();
-            moviesTable.setAutoCreateRowSorter(true);            
-            this.setLayout(new GridBagLayout());
-            GridBagConstraints constraints = new GridBagConstraints();
-            constraints.anchor = GridBagConstraints.WEST;
-            constraints.insets = new Insets(10, 10, 10, 10);
 
-            // SCROLL VIEWER
-            constraints.gridx=1;
-            constraints.gridy=0;    
-            this.add(scrollPane, constraints);
-            scrollPane.setViewportView(moviesTable);
+    public ShowMovieWindow() {
 
-            //BUTTON
-            constraints.gridy=1; 
-            constraints.gridx=1; 
-            constraints.anchor = GridBagConstraints.PAGE_END;
-            this.add(showMovieButton, constraints);    
+        scrollPane = new JScrollPane();
+        showMovieButton = new JButton("Usuń film");
+        moviesTable = new javax.swing.JTable();
+        moviesTable.setAutoCreateRowSorter(true);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.anchor = GridBagConstraints.WEST;
+        constraints.insets = new Insets(10, 10, 10, 10);
 
-            // BORDER
-            this.setBorder(BorderFactory.createTitledBorder(
-            BorderFactory.createEtchedBorder(), "Dostępne pozycje"));
+        // SCROLL VIEWER
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        this.add(scrollPane, constraints);
+        scrollPane.setViewportView(moviesTable);
 
+        //BUTTON
+        constraints.gridy = 1;
+        constraints.gridx = 1;
+        constraints.anchor = GridBagConstraints.PAGE_END;
+        this.add(showMovieButton, constraints);
+
+        // BORDER
+        this.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createEtchedBorder(), "Dostępne pozycje"));
+
+    }
+
+    public JTable getTable() {
+        return this.moviesTable;
+    }
+
+    public static ShowMovieWindow getInstance() {
+        if (instance == null) {
+            instance = new ShowMovieWindow();
         }
-        
-        public JTable getTable(){
-            return this.moviesTable;
-        }
-
-
-        public static ShowMovieWindow getInstance() {
-            if (instance == null) {
-                instance = new ShowMovieWindow();
-            }
-            return instance;
-        }    
+        return instance;
+    }
 }
